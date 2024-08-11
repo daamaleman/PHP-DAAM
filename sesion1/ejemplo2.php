92ea6e7d01fe32dac659e6396b40090c9230f840
Diedereich Alexander Alemán Martínez <164054664+daamaleman@users.noreply.github.com>
2024-08-10 19:31:54 -0600

    Delete sesion1 directory

--- sesion1/ejemplo2.php
+++
@@ -1,7 +0,0 @@
-<?php
-$edad = 17;
-if ($edad >= 18) {
-    echo "Mayor de edad";
-} else {
-    echo "Menor de edad";
-}
--- sesion1/ejemplo4.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-function suma(int $num1, int $num2){
-    return $num1 + $num2;
-}
-$num1 = 5;
-$num2 = 1;
-
-echo "La suma es: " . suma($num1, $num2);
